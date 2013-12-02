3eb6624896916df766b5a2d98d97b99b486d6e37
batesy <gbatesy@gmail.com>
2013-12-02 18:35:35 -0500

    Added Sign Up and Log In links and flash messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Microfunder::Application.routes.draw do
-  get "user_sessions/new"
-  get "user_sessions/create"
-  get "user_sessions/destroy"
+  root :to => 'projects#index'
   resources :projects
   resources :users
-  resources :user_sessions
+  resources :user_sessions, :only => [:new, :create, :destroy]
 
   get 'login' => 'user_sessions#new', :as => :login
   post 'logout' => 'user_sessions#destroy', :as => :logout
