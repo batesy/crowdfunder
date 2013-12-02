1cb1e0996b4ed2b1108e1825f3b46ac1d383d5c7
batesy <gbatesy@gmail.com>
2013-12-02 18:17:49 -0500

    Added validations for Sorcery

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Microfunder::Application.routes.draw do
   resources :projects
   resources :users
+  resources :user_sessions
+
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'logout' => 'user_sessions#destroy', :as => :logout
 end
