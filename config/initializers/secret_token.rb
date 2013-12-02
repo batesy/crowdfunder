22dfabc0abd83657c4c24225480293d616d9eee0
batesy <gbatesy@gmail.com>
2013-12-02 15:47:31 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Microfunder::Application.config.secret_key_base = 'f31407597a1ed9bfa70135870012330dc0d4428b364edbef92ad7dbb44b174bbffa2ac15eb20db7a8ee44208a40f21588ac28c190fa8bff015bfcdbea3642bfe'
