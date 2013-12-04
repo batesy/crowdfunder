cd61f7d759ed48e06e2d7a95c21e2152d3819125
batesy <gbatesy@gmail.com>
2013-12-04 18:43:32 -0500

    Added list of backed projects to user profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ActiveRecord::Base
   has_many :projects
 
   has_many :donations
-  has_many :backed_projects, through: :donations, :class_name => "Projects"
+  has_many :backed_projects, through: :donations, :source => :project 
 
   mount_uploader :image, ImageUploader
 
