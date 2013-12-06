f53257c481ada86c609879525132276b15fb59d2
batesy <gbatesy@gmail.com>
2013-12-05 22:24:26 -0500

    Fixed images on projects index page

--- app/uploaders/project_image_uploader.rb
+++ app/uploaders/project_image_uploader.rb
@@ -25,7 +25,7 @@ class ProjectImageUploader < CarrierWave::Uploader::Base
   # end
 
   #Process files as they are uploaded:
-  # process :scale => [200, 200]
+  # process :resize_to_limit => [500, 500]
   #
   # def scale(width, height)
   #   # do something
@@ -33,7 +33,7 @@ class ProjectImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_limit => [100, 100]
+    process :resize_to_fill => [300, 300]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
